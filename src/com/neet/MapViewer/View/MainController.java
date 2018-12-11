494e2c08311a17e0045432cf54fdc21ae95bc4ff
Looi <https://github.com/looiwenghoong/WallCom.git>
2018-12-11 12:04:00 +0800

    Added animations

--- src/com/neet/MapViewer/View/MainController.java
+++ src/com/neet/MapViewer/View/MainController.java
@@ -3,16 +3,26 @@ package com.neet.MapViewer.View;
 import com.neet.DiamondHunter.Main.Game;
 import com.neet.MapViewer.Main.Cursor;
 import com.neet.MapViewer.Main.MapMain;
+import javafx.animation.PathTransition;
+import javafx.animation.PauseTransition;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+
+import javafx.scene.shape.*;
+import javafx.util.Duration;
 
 
 public class MainController {
 
     @FXML
-    public Label boatId, axeId, cursorId, axeController;
+    public Label boatId, axeId, cursorId;
+
+    @FXML
+    public ImageView axeImage, boatImage;
 
     @FXML
     public void handleKeyPressed (KeyEvent event) {
@@ -74,5 +84,34 @@ public class MainController {
             MapMain.mapViewer.setItem(2);
             boatId.setText("BOAT: (" + MapMain.mapViewer.getBoatRow() + ", " + MapMain.mapViewer.getBoatCol() + ")");
         }
+    }
+
+    public void tryThis()
+    {
+        Path sBoatPath = new Path();
+        sBoatPath.getElements().add(new MoveTo(15, 15));
+        sBoatPath.getElements().add(new VLineTo(10));
+        sBoatPath.getElements().add(new ClosePath());
+
+        PathTransition sBoatPathTransition = new PathTransition();
+        sBoatPathTransition.setDuration(Duration.millis(1000));
+        sBoatPathTransition.setNode(boatImage);
+        sBoatPathTransition.setPath(sBoatPath);
+        sBoatPathTransition.setCycleCount(PathTransition.INDEFINITE);
+        sBoatPathTransition.play();
+
+
+        Path sAxePath = new Path();
+        sAxePath.getElements().add(new MoveTo(0, 15));
+        sAxePath.getElements().add(new VLineTo(10));
+        sAxePath.getElements().add(new ClosePath());
+
+        PathTransition sAxePathTransition = new PathTransition();
+        sAxePathTransition.setDuration(Duration.millis(900));
+        sAxePathTransition.setNode(axeImage);
+        sAxePathTransition.setPath(sBoatPath);
+        sAxePathTransition.setCycleCount(PathTransition.INDEFINITE);
+        sAxePathTransition.play();
+
     }
 }
